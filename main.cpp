66712f7fa7d2f2b8467689d939ce9759a372fa6e
Antony Shibu Paul <antonyshibupaul@Antonys-Laptop.local>
2025-06-20 09:35:48 +0530

    Initial Commit on SortWiz

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+
+int main() {
+    std::vector<int> data = {5, 3, 8, 4, 2};
+
+    // Sorting visualizer logic goes here
+    std::cout << "Sorting Visualizer" << std::endl;
+
+    return 0;
+}
