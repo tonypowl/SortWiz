21d27087b2444e6e557b3cff830d80b333dd97e4
tonypowl <tonypaul0412@gmail.com>
2025-07-10 15:26:18 +0530

    changes made to the main file following the sdl2  code for a plain white screen

--- main.cpp
+++ main.cpp
@@ -1,65 +1,17 @@
+//Using SDL and standard IO
 #include <SDL2/SDL.h>
-#include <iostream>
+#include <stdio.h>
 
-int main() {
-    // Initialize SDL
-    if (SDL_Init(SDL_INIT_VIDEO) != 0) {
-        std::cerr << "SDL Init failed: " << SDL_GetError() << std::endl;
-        return 1;
-    }
+//Screen dimension constants
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
 
-    // Create SDL Window
-    SDL_Window* window = SDL_CreateWindow(
-        "SDL2 Sorting Visualizer",
-        SDL_WINDOWPOS_CENTERED,
-        SDL_WINDOWPOS_CENTERED,
-        800,
-        600,
-        SDL_WINDOW_SHOWN
-    );
+SDL_Window* gWindow = NULL;
+SDL_Surface* gScreenSurface = NULL;
+SDL_Surface* gHelloWorld = NULL;
 
-    if (!window) {
-        std::cerr << "Failed to create window: " << SDL_GetError() << std::endl;
-        SDL_Quit();
-        return 1;
-    }
+int main( int argc, char* args[] )
+{
 
-    // Create SDL Renderer
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    if (!renderer) {
-        std::cerr << "Failed to create renderer: " << SDL_GetError() << std::endl;
-        SDL_DestroyWindow(window);
-        SDL_Quit();
-        return 1;
-    }
-
-    // Event loop
-    bool isRunning = true;
-    SDL_Event event;
-
-    while (isRunning) {
-        while (SDL_PollEvent(&event)) {
-            if (event.type == SDL_QUIT) {
-                isRunning = false;
-            }
-        }
-
-        // Clear the screen with black
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-        SDL_RenderClear(renderer);
-
-        // Draw something (red background rectangle as placeholder)
-        SDL_SetRenderDrawColor(renderer, 255, 0, 0, 255);
-        SDL_Rect rect = {100, 100, 600, 400};
-        SDL_RenderFillRect(renderer, &rect);
-
-        // Present the renderer
-        SDL_RenderPresent(renderer);
-    }
-
-    // Cleanup
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-    return 0;
 }
+
